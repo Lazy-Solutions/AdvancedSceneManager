a7c78f7104eacba0b83e5bccc55655f02cf10828
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2022-06-07 17:52:46 +0200

    Update _build.bat

--- docfx/_build.bat
+++ docfx/_build.bat
@@ -1,2 +1,19 @@
-docfx build
+@echo off
+SetLocal EnableExtensions EnableDelayedExpansion
+
+for /f %%a in ('git rev-parse --abbrev-ref HEAD') do set "branch=%%a"
+
+
+if not !branch! == gh-pages ( 
+echo Must be on gh-pages branch^^!
 PAUSE
+Exit
+)
+
+echo %branch%
+
+
+PAUSE
+
+REM docfx build
+REM PAUSE
